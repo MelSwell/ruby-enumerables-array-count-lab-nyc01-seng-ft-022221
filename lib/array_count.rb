1297f900fdc2be235d90253a27d2bb1535b4c88a
Seth Cohen <smcohen82@gmail.com>
2021-02-08 21:26:22 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,7 +3,7 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count { |element| element == "" }
 end
 
 puts count_strings([1, 2, "seth", 3, "mel"])
